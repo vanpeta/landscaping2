70104b5fd5a866255ec86a13e14ef2b4d2352448
Carlos Allende <carlosallendesanz@gmail.com>
2016-07-14 08:58:11 -0700

    fixing seeds

--- config/seeds.js
+++ config/seeds.js
@@ -5,14 +5,13 @@ var Admin = require('../models/admin');
 
 var admins = {name: 'Testing Admins'};
 
-Admin.remove({}, function(err){
-  console.log('removing db content')
+Admin.remove({}, function (err) {
   if (err) console.log(err);
   Admin.create(admins, function(err, admins) {
     if (err) {
-      console.log (err);
+      console.log(err);
     } else {
-      console.log ('Database seeded with ' + admins.length + "Admins.");
+      console.log("Database seeded with " + admins.length + " admins.");
       mongoose.connection.close();
     }
     process.exit();
