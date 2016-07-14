ee1449e1cbd4fefd35994c3804501c8d3b36406b
Carlos Allende <carlosallendesanz@gmail.com>
2016-07-13 18:08:44 -0700

    express app set

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./config/routes');
+
 
 var app = express();
+
+//load env var
+require('dotenv').load();
+
+// connect to MongoFB with mongoose
+require('./config/database');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
