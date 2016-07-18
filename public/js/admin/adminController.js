449252d4c5fa5e974b44d6a7fd3ecc0b213317c1
Carlos Allende <carlosallendesanz@gmail.com>
2016-07-17 18:22:34 -0700

    admin.html showing images from imgur album

--- public/js/admin/adminController.js
+++ public/js/admin/adminController.js
@@ -12,46 +12,112 @@
     vm.test="testing"
     vm.uploadImageForm
     vm.sendToImgur=sendToImgur
+    var albumDeletehash = "8dTjIP45GplZuc0"
+    var albumId = "fKeE0"
+    vm.images= []
     var imgurClient=''
+    var ids = []
 
-
+// GET Imgur Client Key from the back-end
     $http({
       method: 'GET',
       url: '/api/imgurKey'
-    }). then(function (res) {
-      imgurClient = res.data
-    })
+    }).then(
+      function (res) {
+        imgurClient = res.data
+        getAlbumImages()
+      }
+    );
+
+//CREATE Album
+    function createAlbum () {
+      $http({
+        method: 'POST',
+        url: 'https://api.imgur.com/3/album',
+        data: {'title': 'S & P Hanson Landscaping Professional Landscape Services'},
+        headers: {'Authorization': 'Client-ID '+imgurClient}
+      }).then(
+        function (res) {
+          albumId = res.data.data.id
+          albumDeletehash = res.data.data.deletehash
+          console.log(albumId)
+        }
+      )
+    };
 
     function sendToImgur () {
-      var fileReader = new FileReader();
-      fileReader.readAsArrayBuffer(vm.uploadImageForm);
-      fileReader.onload = function(e) {
-        $http({
-          url: 'upload',
-          header: {'Content-Type': vm.uploadImageForm.type},
-          data: e.target.result
-        }).then (function(response) {
-
-        },null, function(evt) {
-          vm.progress[index] = parseInt(100.0 * evt.loaded / evt.total)
-        });
+      if (!albumId) {
+        console.log('album not detected')
+        createAlbum()
       }
+      else {
+        console.log('album detected')
+//Transform selected image to 64 binary code
+        var fileReader = new FileReader();
+        fileReader.readAsArrayBuffer(vm.uploadImageForm);
+        fileReader.onload = function(e) {
+          $http({
+            url: 'upload',
+            header: {'Content-Type': vm.uploadImageForm.type},
+            data: e.target.result
+          }).then (function(response) {
 
+          },null, function(evt) {
+            vm.progress[index] = parseInt(100.0 * evt.loaded / evt.total)
+          });
+          console.log('image transformation completed')
+        }
+//UPLOAD selected image
+        console.log('uploading image starting')
+        var promise = $http({
+          method: 'POST',
+          url: 'https://api.imgur.com/3/image',
+          data: vm.uploadImageForm,
+          headers: {'Authorization': 'Client-ID '+imgurClient}
+        }).then(
+          function(res) {
+            console.log(res)
+            ids.push(res.data.data.id)
+            console.log("SUCCESS!!! the new image uploaded id is "+ids[0])
+            addImageToAlbum();
+          }
+        );
+      }
+    }
+
+//ADD selected image to Album
+    function addImageToAlbum () {
+      console.log('addImageToAlbum triggered')
       var promise = $http({
-        method: 'POST',
-        url: 'https://api.imgur.com/3/upload',
-        data: vm.uploadImageForm,
+        method: 'PUT',
+        url: 'https://api.imgur.com/3/album/'+albumDeletehash+'/add',
+        data: {'ids': ids},
         headers: {'Authorization': 'Client-ID '+imgurClient}
       });
       promise.then(
         function(res) {
           console.log(res)
-          //console.log(JSON.parse(xhr.responseText).upload.links.imgur_page)
-          console.log('worked')
-          var response = res.data.data.link
-          console.log(response)
+          console.log('image added to album')
+          getAlbumImages()
         }
       )
     }
+
+// GET Album images
+    function getAlbumImages () {
+      console.log('getAlbumImages triggered')
+      $http({
+        method: 'GET',
+        url: 'https://api.imgur.com/3/album/'+albumId+'/images',
+        headers: {'Authorization': 'Client-ID '+imgurClient}
+      }).then(
+        function (res) {
+          vm.images = res.data.data
+          console.log(vm.images)
+        }
+      )
+    };
+
   }
 })();
+
